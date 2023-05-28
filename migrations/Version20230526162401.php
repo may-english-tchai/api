54594f9e39a31ea993f7ac0982235bbff5e1be36
Fahari Hamada Sidi <mkl.devops@gmail.com>
2023-05-28 21:03:39 +0200

    feat: #5 feat install jwt

--- migrations/Version20230526162401.php
+++ migrations/Version20230526162401.php
@@ -14,7 +14,7 @@ final class Version20230526162401 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Init all entities';
     }
 
     public function up(Schema $schema): void
