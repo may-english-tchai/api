bae24b3107b777a6d86a795067c6a8e60ba2024d
eddy <boughanmieddy@outlook.fr>
2023-06-19 15:54:15 +0200

    version 1

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -11,10 +11,24 @@ use Doctrine\ORM\Mapping as ORM;
 #[ApiResource(
     operations: [
         new Post(security: 'is_granted("PUBLIC_ACCESS")'),
+        new Get,
     ]
 )]
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
 class Contact extends Content
 {
     use EmailEntityTrait;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $phone;
+
+    public function getphone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setphone(?string $phone): void
+    {
+        $this->phone = $phone;
+    }
 }
