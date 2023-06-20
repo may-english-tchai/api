1100cc4e755a74126bea59deb0de827aa4982a0e
EddyBough <121858537+EddyBough@users.noreply.github.com>
2023-06-20 10:58:58 +0200

    version 1 get phone number on contact page (#66)
    * version 1
    
    * latest version

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Post;
 use App\Repository\MessageRepository;
 use App\Trait\EmailEntityTrait;
@@ -11,10 +12,24 @@ use Doctrine\ORM\Mapping as ORM;
 #[ApiResource(
     operations: [
         new Post(security: 'is_granted("PUBLIC_ACCESS")'),
+        new Get(),
     ]
 )]
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
 class Contact extends Content
 {
     use EmailEntityTrait;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $phone = null;
+
+    public function getphone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setphone(?string $phone): void
+    {
+        $this->phone = $phone;
+    }
 }
