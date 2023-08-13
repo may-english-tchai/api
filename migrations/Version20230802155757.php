94134e972469288c45c262308b92e18302af7814
eddy <boughanmieddy@outlook.fr>
2023-08-13 13:20:44 +0200

    update password changes

---
+++ migrations/Version20230802155757.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230802155757 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE config (id BLOB NOT NULL --(DC2Type:uuid)
+        , name VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, type VARCHAR(20) NOT NULL, is_enabled BOOLEAN DEFAULT 1 NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D48A2F7C5E237E06 ON config (name)');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__payment AS SELECT id, participation_id, reference, amount, comment, created_at, updated_at, deleted_at, status, data FROM payment');
+        $this->addSql('DROP TABLE payment');
+        $this->addSql('CREATE TABLE payment (id BLOB NOT NULL --(DC2Type:uuid)
+        , participation_id BLOB NOT NULL --(DC2Type:uuid)
+        , reference VARCHAR(255) DEFAULT NULL, amount DOUBLE PRECISION NOT NULL, comment CLOB DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, status VARCHAR(255) NOT NULL, data CLOB NOT NULL --(DC2Type:json)
+        , PRIMARY KEY(id), CONSTRAINT FK_6D28840D6ACE3B73 FOREIGN KEY (participation_id) REFERENCES participation (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO payment (id, participation_id, reference, amount, comment, created_at, updated_at, deleted_at, status, data) SELECT id, participation_id, reference, amount, comment, created_at, updated_at, deleted_at, status, data FROM __temp__payment');
+        $this->addSql('DROP TABLE __temp__payment');
+        $this->addSql('CREATE INDEX IDX_6D28840D6ACE3B73 ON payment (participation_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_6D28840DAEA34913 ON payment (reference)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE config');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__payment AS SELECT id, participation_id, status, reference, data, amount, comment, deleted_at, created_at, updated_at FROM payment');
+        $this->addSql('DROP TABLE payment');
+        $this->addSql('CREATE TABLE payment (id BLOB NOT NULL --(DC2Type:uuid)
+        , participation_id BLOB NOT NULL --(DC2Type:uuid)
+        , status VARCHAR(255) NOT NULL, reference VARCHAR(255) NOT NULL, data CLOB NOT NULL --(DC2Type:json)
+        , amount DOUBLE PRECISION NOT NULL, comment CLOB DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id), CONSTRAINT FK_6D28840D6ACE3B73 FOREIGN KEY (participation_id) REFERENCES participation (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO payment (id, participation_id, status, reference, data, amount, comment, deleted_at, created_at, updated_at) SELECT id, participation_id, status, reference, data, amount, comment, deleted_at, created_at, updated_at FROM __temp__payment');
+        $this->addSql('DROP TABLE __temp__payment');
+        $this->addSql('CREATE INDEX IDX_6D28840D6ACE3B73 ON payment (participation_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_6D28840DAEA34913 ON payment (reference)');
+    }
+}
